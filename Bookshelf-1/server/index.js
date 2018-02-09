b659e196363d3ba85d0ebb81c4748fcc7309d190
Austin Wright <awguitar86@gmail.com>
2018-02-09 15:59:50 -0700

    called listen on app to listen on port 3000

--- Bookshelf-1/server/index.js
+++ Bookshelf-1/server/index.js
@@ -3,5 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
+
+app.listen( 3000, () => console.log("Server is listening on port 3000") );
 
