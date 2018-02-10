961e088e5499d06ac8bc42a50a409a73ede81e0d
Austin Wright <awguitar86@gmail.com>
2018-02-09 17:06:43 -0700

    changes

--- Bookshelf-1/server/controllers/books_controller.js
+++ Bookshelf-1/server/controllers/books_controller.js
@@ -1,33 +1,36 @@
-var books = [];
-var id = 0;
+let books = [];
+let id = 0;
 
 module.exports = {
-    create: ( req, res ) => {
-        const { title, author } = req.body;
-        books.push( { id, title, author } );
-        id++;
-        res.status(200).send( books );
-    }, 
-    read: ( req, res ) => {
-        res.status(200).send( books );
-    },
-    update: ( req, res ) => {
-        const updateID = req.params.id;
-        let index = books.findTindex( book => book.id == updateID );
+  create: ( req, res ) => {
+    const { title, author } = req.body;
+    books.push( { id, title, author } );
+    id++;
+    res.status(200).send( books );
+  },
 
-        books[ index ] = {
-            id: books[ index ].id,
-            title: req.body.title || books[ index ].title,
-            author: req.body.author || books[ index ].author
-        };
+  read: ( req, res ) => {
+    res.status(200).send( books );
+  },
 
-        res.status(200).send( books );
-    },
-    delete: ( req, res ) => {
-        const deleteID = req.params.id;
-        bookID = books.findIndex( book => book.id == deleteID );
-        books.splice( bookID, 1 );
-        res.status(200).send( books );
-    }
+  update: ( req, res ) => {
+    const updateID = req.params.id;
+    let index = books.findIndex( book => book.id == updateID );
+
+    books[ index ] = {
+      id: books[ index ].id,
+      title: req.body.title || books[ index ].title,
+      author: req.body.author || books[ index ].author
+    };
+
+    res.status(200).send( books );
+  },
+
+  delete: ( req, res ) => {
+    const deleteID = req.params.id;
+    bookID = books.findIndex( book => book.id == deleteID );
+    books.splice( bookID, 1 );
+    res.status(200).send( books );
+  }
 };
 
--- Bookshelf-1/server/index.js
+++ Bookshelf-1/server/index.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var bc = require(__dirname + '/controllers/books_controller.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const bc = require( __dirname + '/controllers/books_controller');
 
-var app = express();
+const app = express();
 
 app.use( bodyParser.json() );
-app.use( express.static( __dirname + "/../public.build") );
+app.use( express.static( __dirname + "/../public/build") );
 
 const baseURL = "/api/books";
-app.post( baseURL, bc.create );
-app.get( baseURL, bc.read );
-app.put( `${baseURL}/:id`, bc.update );
-app.delete( `${baseURL}/:id`, bc.delete);
+app.post(baseURL, bc.create);
+app.get(baseURL, bc.read);
+app.put(`${baseURL}/:id`, bc.update);
+app.delete(`${baseURL}/:id`, bc.delete);
 
 const port = 3000;
-app.listen( port, () => console.log(`Server is listening on port ${port}`) );
-
+app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
