01753c09aa1803be91f442ba106e600a3a3a428e
Austin Wright <awguitar86@gmail.com>
2018-02-21 17:19:20 -0700

    used express.static on app to build and launch app

--- Bookshelf-2/server/index.js
+++ Bookshelf-2/server/index.js
@@ -4,15 +4,17 @@ const bc = require('./controllers/books_controller');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
 
-app.post('/api/books', bc.create);
+app.use( express.static( __dirname + '/../public/build') );
 
-app.get('/api/books', bc.read);
+app.post( '/api/books', bc.create );
 
-app.put('/api/books/:id', bc.update);
+app.get( '/api/books', bc.read );
 
-app.delete('/api/books/:id', bc.delete);
+app.put( '/api/books/:id', bc.update );
+
+app.delete( '/api/books/:id', bc.delete );
 
 const port = 3000;
 app.listen(port, () => { console.log(`Server is listening on port ${port}`); });
