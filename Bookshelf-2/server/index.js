e9eeaaca984d854e9faab1de5d97294bf8424a66
Austin Wright <awguitar86@gmail.com>
2018-02-21 17:08:29 -0700

    required books controller, and created routes on app

--- Bookshelf-2/server/index.js
+++ Bookshelf-2/server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bc = require('./controllers/books_controller');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('/api/books', bc.create);
+
+app.get('/api/books', bc.read);
+
+app.put('/api/books/:id', bc.update);
+
+app.delete('/api/books/:id', bc.delete);
 
 const port = 3000;
 app.listen(port, () => { console.log(`Server is listening on port ${port}`); });
